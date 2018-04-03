1a9b779c7605b51023e2080a9c675a7090dfaae2
nubochistka <33056242+nubochistka@users.noreply.github.com>
2018-04-03 10:53:03 +0300

    Update settining_db.sh

--- settining_db.sh
+++ settining_db.sh
@@ -17,4 +17,4 @@ docker build -t $NAME_IMAGE apache
 docker tag $NAME_IMAGE $REPO_DOCKER/$NAME_IMAGE
 docker push $REPO_DOCKER/$NAME_IMAGE
 
-sed -i='' "s/<IMAGE_REPO>/$REPO_DOCKER\/$NAME_IMAGE/" Dockerrun.aws.json
+sed -i "s/<IMAGE_REPO>/$REPO_DOCKER\/$NAME_IMAGE/" Dockerrun.aws.json
